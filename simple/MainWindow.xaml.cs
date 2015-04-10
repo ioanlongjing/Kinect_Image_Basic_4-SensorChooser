aba63ef772601a103404b269223303657ad52c38
ioan.snickersnee@gmail.com <ioan.snickersnee@gmail.com>
2015-04-10 14:20:25 +0800

    Simple

---
+++ simple/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using System.IO;
+
+namespace Joint
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor sensor;
+        private byte[] colorPixels;
+        private WriteableBitmap colorBitmap;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+            if (null != this.sensor)
+            {
+                this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);//取得畫面
+
+                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];//安排記憶體
+
+                this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth,
+                                                       this.sensor.ColorStream.FrameHeight,
+                                                       96.0, 96.0, PixelFormats.Bgr32, null);
+
+                this.image1.Source = this.colorBitmap; //丟到Image
+
+                this.sensor.AllFramesReady += this.sensor_AllFramesReady;
+
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+            else
+            {
+                //Lable1.Content = "沒有連接";
+                label1.Content = Properties.Resources.NoKinectReady;
+            }
+        }
+        void sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (colorFrame != null)
+                {
+                    // Copy the pixel data from the image to a temporary array
+                    colorFrame.CopyPixelDataTo(this.colorPixels);
+
+                    // Write the pixel data into our bitmap
+                    this.colorBitmap.WritePixels(
+                        new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                        this.colorPixels,
+                        this.colorBitmap.PixelWidth * sizeof(int),
+                        0);
+                }
+            }
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.sensor != null)
+                this.sensor.Stop();
+        }
+
+
+    }
+}
